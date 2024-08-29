ebcc95ffa01ea124d9866b6c4dded1676ede380e
miguel whyte <whytemig@gmail.com>
2024-08-28 23:31:08 -0400

    initialize firebase & read docs

---
+++ src/library/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "cringe-app-32ea4.firebaseapp.com",
+  projectId: "cringe-app-32ea4",
+  storageBucket: "cringe-app-32ea4.appspot.com",
+  messagingSenderId: "673303406790",
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: "G-ENMVQK36VX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication, Database & Storage for Images and get a reference to the service
+export const auth = getAuth(app);
+export const database = getFirestore(app);
+export const storage = getStorage(app);
