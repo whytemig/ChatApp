736b91b89db5ac4ccbcf7f98a71c8f9983c8d522
miguel whyte <whytemig@gmail.com>
2024-08-29 00:08:07 -0400

    updates

--- src/library/firebase.js
+++ src/library/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "cringe-app-32ea4.firebaseapp.com",
